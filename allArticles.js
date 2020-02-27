144638fbf6c7c70ea465cbbcdb127083ab1a355f
DanielleInkster <danielle.inkster@gmail.com>
2020-02-27 01:40:36 +0000

    iterates through all articles

--- allArticles.js
+++ allArticles.js
@@ -5,12 +5,13 @@ let url = 'http://newsapi.org/v2/top-headlines?' +
           'country=us&' +
           'apiKey=1841c0b4b8ec4bbba16000ceaef85c30';
 fetch(url)
-    .then(function(response) {
+    .then((response) =>{
         return response.json();
     }).then((data) => {
-    console.log(data);
-    output.innerHTML="<center><img src="+`${data.articles[0].urlToImage}`+" width='280' height='180'></img>"+
-                    "<h1>"+`${data.articles[0].title}`+"</h1>" +
-                    "<h3>"+ `${data.articles[0].content.substring(0, 230)}`+"... </h3> </center>" 
+    data.articles.forEach(article =>
+    output.innerHTML += "<center><img src="+`${article.urlToImage}`+" width='280' height='180'></img>"+
+                    "<h1>"+`${article.title}`+"</h1>" +
+                    "<h3>"+ `${article.description}`+"... </h3> </center> <br>" 
+    )
   })
 }
