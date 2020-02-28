86517253c2b07aa03941f8b0d54e81b98c03f576
DanielleInkster <danielle.inkster@gmail.com>
2020-02-28 22:36:35 +0000

    btn listens for event / incorrect display

--- allArticles.js
+++ allArticles.js
@@ -2,7 +2,7 @@
 function getArticles(){
 let output = document.querySelector('.output')
 let url = 'http://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
+          'sources=bbc-news&' +
           'apiKey=1841c0b4b8ec4bbba16000ceaef85c30';
 fetch(url)
     .then((response) =>{
@@ -10,12 +10,20 @@ fetch(url)
     }).then((data) => {
     data.articles.forEach(article =>
     output.innerHTML += "<center><img src="+`${imageUrl(article.urlToImage)}`+" width='280' height='180'></img>"+
-                    "<h1>"+`${article.title}`+"</h1>" +
-                    "<h3>"+ `${article.description.substring(0, 100)}`+"... </h3> </center> <br>" 
+                    "<h1> <a href="+ `${article.url}`+">" +`${article.title}`+"</a></h1>" +
+                    // "<h3>"+ `${article.description.substring(0, 100)}`+"... </h3>"+
+                    "<button id = 'btn' onclick= "+`${readArticle(article)}`+"'> See More </button>" + "</center><br>" 
     )
+    document.getElementById('btn').addEventListener('click', readArticle(), false);
   })
 }
 
+
+function readArticle(article){
+  let output2 = document.querySelector('.output2')
+  output2.innerHTML = "<h2>"+`${article.title}`+"<h2> <br> <p>" + `${article.description}`+"<p>"
+}
+
 function imageUrl(img){
- return img===null ? url = 'img/no-image.png' : url = img
+ return img===null ?  'img/no-image.png' : img
 }
