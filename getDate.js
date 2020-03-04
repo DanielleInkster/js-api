02b12621d34d30b6c75ce643e90eff91b6f739de
DanielleInkster <danielle.inkster@gmail.com>
2020-03-04 23:41:41 +0000

    Setup bootstrap

--- getDate.js
+++ getDate.js
@@ -2,7 +2,7 @@ var options = {  weekday: 'long', year: 'numeric',
                  month: 'long', day: 'numeric', hour: '2-digit',
                  minute: '2-digit', second: '2-digit' };
 
-let date = document.querySelector('.date')
+let date = document.querySelector('#date')
 
 function prnDt(opt = options){
     date.innerHTML = new Date().toLocaleTimeString('en-us', opt)
