c1f3a6891c09e922c2a1b8c0fbb106b97d05778e
DanielleInkster <danielle.inkster@gmail.com>
2020-03-06 22:52:23 +0000

    Refactor repeating bootstrap"

--- allArticles.js
+++ allArticles.js
@@ -12,10 +12,10 @@ function getArticles(){
           return response.json();
       }).then((data) => {
       data.articles.forEach(function(article, index){
-            output.innerHTML += "<div class = 'card bg-secondary p-2'><img class ='rounded mx-auto d-block' id = 'articleImage1'" +
+            output.innerHTML += "<div class = 'card bg-secondary rounded p-2 mx-auto'><img class ='rounded mx-auto d-block' id = 'articleImage1'" +
                                 "src="+`${imageUrl(article.urlToImage)}`+"></img>"+
-                                "<h1 class = 'text-center' id = 'sidebarTitle'> " +`${article.title}`+"</a></h1>" +
-                                "<h2 class = 'text-center' id = 'sidebarArticle'>"+ `${article.description.substring(0, 100)}`+"... </h2>"+
+                                "<h1 id ='sidebarTitle'> " +`${article.title}`+"</a></h1>" +
+                                "<h2 id ='sidebarArticle'>"+ `${article.description.substring(0, 100)}`+"... </h2>"+
                                 "<button id='btn' onclick ='readArticle("+`${index}` +");'> See More </button>" + 
                                 "</div><br>"                             
       } )
@@ -32,9 +32,9 @@ function readArticle(index){
     .then((response) =>{
         return response.json();
     }).then((data) => {
-    output2.innerHTML = "<center><div class = ' card bg-secondary rounded p-2 mx-auto'><img class ='rounded' id='articleImage2'"+
+    output2.innerHTML = "<center><div class = 'card bg-secondary rounded p-2 mx-auto'><img class ='rounded' id='articleImage2'"+
                         "src="+`${imageUrl(data.articles[index].urlToImage)}`+"></img>"+
-                        "<br class='articleBreak'> <h1 id ='title2'><a class= 'text-white' href="+ `${data.articles[index].url}`+">"+
+                        "<br class='articleBreak'> <h1 id ='title2'><a class='text-white' href="+ `${data.articles[index].url}`+">"+
                         `${data.articles[index].title}`+"</a></h1>"+
                         "<h4 id='article'>" + `${data.articles[index].content}`+"<h4></div></center>"
   })
